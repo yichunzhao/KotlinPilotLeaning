b7478874911ef3b3fdb90648fc3bdf7891e350d1
yichunzhao <zhaoyichun@hotmail.com>
2019-10-14 00:50:43 +0200

    kotlin fun = java method; can ignoring void; can live outside class

---
+++ src/kotlinFun/funDemo.kt
@@ -0,0 +1,11 @@
+package kotlinFun
+
+//void --> Unit in Kotlin, can be ignored.
+fun main(args: Array<String>) {
+    printAString("hello world")
+}
+
+//kotlin may ignore void(unit)
+fun printAString(value: String) {
+    println(value)
+}
