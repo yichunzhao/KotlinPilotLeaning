9485dffeb5ed97efc90cd809c6a4893dab195ef3
yichunzhao <zhaoyichun@hotmail.com>
2019-10-19 22:58:50 +0200

    kotlin may provide default value to a fun parameter

--- src/kotlinFun/AddTwoNum.kt
+++ src/kotlinFun/AddTwoNum.kt
@@ -1,7 +1,8 @@
 package kotlinFun
 
 //single expression function, can have single only statement
-fun addTwoNumber(num1: Double, num2: Double): Double = num1 + num2
+//may setting a default value for a parameter; this may be achieved by overloading in java
+fun addTwoNumber(num1: Double, num2: Double = 3.8): Double = num1 + num2
 
 fun main() {
     printAString(addTwoNumber(12.2, 13.4).toString())
@@ -12,4 +13,7 @@ fun main() {
 
     //parameter equals can change the parameter order
     println(addTwoNumber(num2 = 23.0, num1 = 12.0))
+
+    //providing num1 alone
+    println(addTwoNumber(num1 = 23.0))
 }
