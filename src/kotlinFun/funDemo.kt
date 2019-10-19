85075b7228684056ce57b73cbeab564dd41c88a6
yichunzhao <zhaoyichun@hotmail.com>
2019-10-19 21:06:26 +0200

    kotlin, non-nullable variable by default

--- src/kotlinFun/funDemo.kt
+++ src/kotlinFun/funDemo.kt
@@ -2,10 +2,17 @@ package kotlinFun
 
 //void --> Unit in Kotlin, can be ignored.
 fun main(args: Array<String>) {
-    printAString("hello world")
+    //by default, in kotlin variable is un-nullable
+    //val name = "my name"
+    //nullable variable must be defined explicitly.
+    val name: String? = null
+    printAString(name)
+
+    //this is a non-nullable variable, it won't accept null value.
+    val anotherName: String  = ""
 }
 
 //kotlin may ignore void(unit)
-fun printAString(value: String) {
+fun printAString(value: String?) {
     println(value)
 }
