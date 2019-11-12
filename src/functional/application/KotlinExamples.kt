d586e5fd8ec91c00a55ad60b43be9ab4443d0ed0
yichunzhao <zhaoyichun@hotmail.com>
2019-11-12 23:42:16 +0100

    Kotlin function reflection

--- src/functional/application/KotlinExamples.kt
+++ src/functional/application/KotlinExamples.kt
@@ -4,8 +4,15 @@ fun applyFunctionToString(input: String, myFunction: (String) -> String): String
     return myFunction(input)
 }
 
+fun convertStringCapital(input: String):String{
+    return input.toUpperCase()
+}
+
 fun main() {
     val input = "hello world"
     val applied = applyFunctionToString(input) { x -> x[0].toUpperCase() + x.substring(1) }
     println(applied)
+
+    //function reflection
+    println(applyFunctionToString(input,::convertStringCapital))
 }
